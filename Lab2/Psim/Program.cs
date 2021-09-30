15dd30ddc1db012f6b3889703b7c936922f3e74c
prince <A00229731@mycambrian.ca>
2021-09-30 01:19:26 -0400

    Lab 2 work is on

---
+++ Lab2/Psim/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Psim.Geometry2D;
+using Psim.Particles;
+
+namespace Psim
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Phonon p = new Phonon(1);
+            Console.WriteLine(p);
+
+            Point point = p.Position;
+            point.SetCoords(100, 100);
+            Console.WriteLine(p);
+        }
+    }
+}
