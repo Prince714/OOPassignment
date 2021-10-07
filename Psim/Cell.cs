ef8996b5f9b5a05c2725418afc20d97023bbd14b
prince <A00229731@mycambrian.ca>
2021-10-07 13:37:05 -0400

    Lab 3 file

---
+++ Psim/Cell.cs
@@ -0,0 +1,93 @@
+﻿
+
+using System;
+using System.Collections.Generic;
+
+using Psim.Geometry2D;
+using Psim.Particles;
+
+namespace Psim.Modelcomponents
+{
+	public enum SurfaceLocation
+	{
+		left =0,
+		top =1,
+		right =2,
+		bot =3,
+	}
+
+	public class Cell : Rectangle
+	{
+		private const int NUM_SURFACES = 4;
+		private List<Phonon> phonons = new() { };
+		private List<Phonon> incomingPhonons = new() { };
+		private ISurface[] surfaces = new ISurface[NUM_SURFACES];
+		public List<Phonon> Phonons { get { return phonons; } }
+
+		public Cell(double length, double width)
+			: base(length, width)
+		{
+			foreach (int surface in Enum.GetValues(typeof(SurfaceLocation)))
+			{
+				surfaces[surface] = new BoundarySurface((SurfaceLocation)surface, this);
+			}
+		}
+		
+
+		/// <summary>
+		/// Adds a phonon to the main phonon 'array' of the cell.
+		/// </summary>
+		/// <param name="p">The phonon that will be added</param>
+		public void AddPhonon(Phonon p)
+		{
+			phonons.Add(p);
+		}
+
+		/// <summary>
+		/// Adds a phonon to the incoming phonon 'array' of the cell
+		/// The incoming phonon will come from the phonons 'array' of another cell
+		/// </summary>
+		/// <param name="p">The phonon that will be added</param>
+		public void AddIncPhonon(Phonon p)
+		{
+			incomingPhonons.Add(p);
+		}
+
+		/// <summary>
+		/// Merges the incoming phonons with the existing phonons and clears the incoming phonons
+		/// </summary>
+		public void MergeIncPhonons()
+		{
+			Phonons.AddRange(incomingPhonons);
+			incomingPhonons.Clear();
+		}
+
+		/// <summary>
+		/// Returns the surface at SurfaceLocation loc
+		/// </summary>
+		/// <param name="loc">The SurfaceLocation of the surface to be returned</param>
+		/// <returns>The surface at location loc</returns>
+		public ISurface GetSurface(SurfaceLocation loc)
+		{
+			return surfaces[(int)loc];
+		}
+
+		/// <summary>
+		/// Moves a phonon to the surface that it will impact first.
+		/// The phonon will be moved to the surface and the surface
+		/// it impacts is returned
+		/// </summary>
+		/// <param name="p">The phonon to be moved</param>
+		/// <returns>The surface that the phonon collides with</returns>
+		public SurfaceLocation? MoveToNearestSurface(Phonon p)
+		{
+			// TODO - challenging!! be cautious of floating point issues!
+			throw new NotImplementedException();
+		}
+
+		public override string ToString()
+		{
+			return string.Format("{1,-7} {2,-7}", phonons.Count, incomingPhonons.Count);
+		}
+	}
+}
---
+++ Psim/Geometry2DUnitTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Geometry2DUnitTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Geometry2DUnitTest")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("feca6387-a4ee-4b1c-ae80-81fc8b6f39ea")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
